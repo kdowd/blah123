315d708da6125d107b63f22c79d55ef921e3ead5
kdowd <kevin.dowd@gmail.com>
2023-08-22 14:22:48 +1200

    qwerty

--- blah123.cpp
+++ blah123.cpp
@@ -2,8 +2,38 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void test(vector<pair<int, int> > &xxx) {
+	//xxx[4]. = 99;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// xxxxxx
+ 
+	std::vector<pair<int, int> > pairs = { {4, 2}, {1, 4}, {5, 6}, make_pair(10, 20) };
+
+	std::vector<pair<string, int> > patest = { {"bb", 2}, {"gg", 4}, {"aa", 6}, make_pair("cc", 20)};
+
+	pairs.emplace_back(7, 8);
+	pairs.emplace_back(make_pair(10, 20));
+	sort(patest.begin(), patest.end());
+
+
+	 
+
+	test(pairs);
+
+	for (auto i : patest) {
+		cout << i.first << "\n";
+	}
+	 
+
+	cin.get();
 }
